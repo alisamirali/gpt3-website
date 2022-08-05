18745fc7a7807017f4812dace518c6f2b201783e
Ali Samir <alisamir.2020@azhar.edu.eg>
2022-08-05 14:18:13 +0200

    Install GitHub Pages

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import gpt3Logo from "../../logo.svg";
+import "./Footer.css";
+
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">
+        Do You Want To Step Into The Future Before Others
+      </h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>
+          Ali Samir <br /> All Rights Reserved
+        </p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Cairo, Egypt</p>
+        <p>+20-111-418-1142</p>
+        <p>alisamir.dev@gmail.com</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2022 GPT-3. All Rights Reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
